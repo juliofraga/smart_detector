d6ce678bf94ab29cd4888e58ba39298942521dc1
juliofraga <julio_fraga10@hotmail.com>
2025-09-04 17:27:59 -0300

    feat: Creating Alert component

--- resources/js/app.js
+++ resources/js/app.js
@@ -27,7 +27,7 @@ const store = new Vuex.Store({
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+Vue.component('alert-component', require('./components/Alert.vue').default);
 Vue.component('login-component', require('./components/Login.vue').default);
 
 /**
---
+++ resources/js/components/Alert.vue
@@ -0,0 +1,23 @@
+<template>
+    <div :class="style" role="alert">
+        {{ title }}
+        <div v-if="type =='danger'">
+            <hr>
+            <p>{{ details.message }}</p>
+            <li v-for="d, key in details.data" :key="key">
+                {{ d[0]}}
+            </li>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['type', 'title', 'details'],
+        computed: {
+            style() {
+                return 'alert alert-' + this.type;
+            }
+        }
+    }
+</script>
