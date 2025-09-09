1a8ea100b465d646ab308f8248afb34f19c9c85b
juliofraga <julio_fraga10@hotmail.com>
2025-09-09 15:50:28 -0300

    ref: Removing unnecessary code

--- resources/js/components/Home.vue
+++ resources/js/components/Home.vue
@@ -21,52 +21,6 @@
                     :feedbackTitle="feedbackTitle"
                     sectionTitle="Eventos Recentes"
                 ></table-component>
-                <!--<div class="card p-3">
-                    <h5 class="mb-3">Eventos Recentes</h5>
-                    <div class="table-responsive">
-                        <table class="table table-dark-custom table-hover align-middle">
-                            <thead>
-                                <tr>
-                                    <th>Descrição</th>
-                                    <th>IP Origem</th>
-                                    <th>Tipo</th>
-                                    <th>Classificação</th>
-                                    <th>Data/Hora</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td>Lorem ipsum dolor sit amet consectetur adipiscing elit.</td>
-                                    <td>192.168.0.15</td>
-                                    <td>Brute Force</td>
-                                    <td><span class="badge badge-danger">ALTO RISCO</span></td>
-                                    <td>2025-08-28 19:22</td>
-                                </tr>
-                                <tr>
-                                    <td>Lorem ipsum dolor sit amet consectetur adipiscing elit.</td>
-                                    <td>201.55.23.87</td>
-                                    <td>SQL Injection</td>
-                                    <td><span class="badge badge-warning">MÉDIO RISCO</span></td>
-                                    <td>2025-08-28 19:22</td>
-                                </tr>
-                                <tr>
-                                    <td>Lorem ipsum dolor sit amet consectetur adipiscing elit.</td>
-                                    <td>45.12.98.10</td>
-                                    <td>Acesso</td>
-                                    <td><span class="badge badge-warning">MÉDIO RISCO</span></td>
-                                    <td>2025-08-28 19:22</td>
-                                </tr>
-                                <tr>
-                                    <td>Lorem ipsum dolor sit amet consectetur adipiscing elit.</td>
-                                    <td>177.45.90.1</td>
-                                    <td>XSS</td>
-                                    <td><span class="badge badge-success">BAIXO RISCO</span></td>
-                                    <td>2025-08-28 19:22</td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
-                </div>-->
             </div>
             <div v-else-if="loaded === true">
                 <no-itens-component></no-itens-component>
