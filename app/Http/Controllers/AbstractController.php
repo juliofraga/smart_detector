8db7e2b61ae068a423073404ef262837f0f62c6c
juliofraga <julio_fraga10@hotmail.com>
2025-09-15 21:44:50 -0300

    ref: Updating UserController to use AbstractController

---
+++ app/Http/Controllers/AbstractController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
+
+abstract class AbstractController extends Controller
+{
+    
+    protected $model;
+
+    public function __construct(Model $model)
+    {
+        $this->model = $model;
+    }
+    
+    public function store(Request $request): JsonResponse
+    {
+        $request->validate($this->model->rules(), $this->model->feedback());
+        $data = $this->model->create($request->all());
+        return parent::response($data);
+    }
+
+    public function update(Request $request, int $id): JsonResponse
+    {
+        $except = [];
+        if ($request->password) {
+            $request->merge([
+                'password' => bcrypt($request->password)
+            ]);
+        } else {
+            $except[] = 'password';
+        }
+        $data = $this->model->find($id);
+        if (!$data) {
+            return parent::responseDataNotFound();
+        }
+        $update = empty($except) ? $data->update($request->all()) : $data->update($request->except($except));
+        return parent::responseOther($update, 'update');
+    }
+
+    public function destroy(int $id): JsonResponse
+    {
+        $model = $this->model->find($id);
+        if (!$model) {
+            return parent::responseDataNotFound();
+        }
+        $delete = $model->delete();
+        return parent::responseOther($delete, 'delete');
+    }
+
+    public function paginate(Request $request, int $qtd = null, array $order): JsonResponse
+    {
+        $by = $order[0];
+        $direction = $order[1];
+        $qtd = $qtd ?? 20;
+        $data = [];
+        if($request->has('filter')) {
+            $this->filter($request->filter);
+        }
+        $data = $this->model->orderby($by, $direction)->paginate($qtd);
+        return parent::responseGeneric($data);
+    }
+
+    public function filter(string $filters): void
+    {
+        $filters = explode(';', $filters);
+        $this->model = $this->model->where(function ($query) use ($filters) {
+            foreach ($filters as $key => $condition) {
+                $c = explode(':', $condition);
+                $query->orWhere($c[0], $c[1], $c[2]);
+            }
+        });
+    }
+
+    abstract public function index(Request $request): JsonResponse;
+
+    abstract public function show();
+
+    
+    
+}
--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,87 +2,42 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
+use App\Http\Controllers\AbstractController;
 use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Http\JsonResponse;
 
-class UserController extends Controller
+class UserController extends AbstractController
 {
-    private $model;
 
     public function __construct(User $user)
     {
-        $this->model = $user;
+        parent::__construct($user);
     }
 
     public function store(Request $request): JsonResponse
     {
-        $request->validate(User::rules(), User::feedback());
         if ($request->password) {
             $request->merge([
                 'password' => bcrypt($request->password)
             ]);
         }
-        $user = $this->model->create($request->all());
-        return parent::response($user);
+        return parent::store($request);
     }
 
     public function update(Request $request, int $id): JsonResponse
     {
-        $except = [];
-        if ($request->password) {
-            $request->merge([
-                'password' => bcrypt($request->password)
-            ]);
-        } else {
-            $except[] = 'password';
-        }
-        $user = $this->model->find($id);
-        if (!$user) {
-            return parent::responseDataNotFound();
-        }
-        $update = empty($except) ? $user->update($request->all()) : $user->update($request->except($except));
-        return parent::responseOther($update, 'update');
+        return parent::update($request, $id);
     }
 
-    public function destroy(int $id)
+    public function destroy(int $id): JsonResponse
     {
-        $model = $this->model->find($id);
-        if (!$model) {
-            return parent::responseDataNotFound();
-        }
-        $delete = $model->delete();
-        return parent::responseOther($delete, 'delete');
+        return parent::destroy($id);
     }
 
     public function index(Request $request): JsonResponse
     {
-        return $this->paginate($request, null, ['name', 'asc']);
-    }
-
-    public function paginate(Request $request, int $qtd = null, array $order): JsonResponse
-    {
-        $by = $order[0];
-        $direction = $order[1];
-        $qtd = $qtd ?? 20;
-        $data = [];
-        if($request->has('filter')) {
-            $this->filter($request->filter);
-        }
-        $data = $this->model->orderby($by, $direction)->paginate($qtd);
-        return parent::responseGeneric($data);
-    }
-
-    public function filter(string $filters): void
-    {
-        $filters = explode(';', $filters);
-        $this->model = User::where(function ($query) use ($filters) {
-            foreach ($filters as $key => $condition) {
-                $c = explode(':', $condition);
-                $query->orWhere($c[0], $c[1], $c[2]);
-            }
-        });
+        return parent::paginate($request, null, ['name', 'asc']);
     }
 
     public function show()
