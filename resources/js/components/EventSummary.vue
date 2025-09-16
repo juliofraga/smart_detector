cececcda6b1d390d3d5901c1bd6e66989070758c
juliofraga <julio_fraga10@hotmail.com>
2025-09-16 16:37:53 -0300

    ref: Updating Event summary tu use the correct classification

--- resources/js/components/EventSummary.vue
+++ resources/js/components/EventSummary.vue
@@ -7,28 +7,16 @@
                     <h2 class="highlight">{{ today }}</h2>
                 </div>
             </div>
-            <div class="col-md-3">
+            <div class="col-md-3" v-for="item in classificationSummary" :key="item.description">
                 <div class="card p-3 text-center">
-                    <h5>Risco Alto</h5>
-                    <h2 class="text-danger">{{ high }}</h2>
-                </div>
-            </div>
-            <div class="col-md-3">
-                <div class="card p-3 text-center">
-                    <h5>Risco Médio</h5>
-                    <h2 class="text-warning">{{ medium }}</h2>
-                </div>
-            </div>
-            <div class="col-md-3">
-                <div class="card p-3 text-center">
-                    <h5>Risco Baixo</h5>
-                    <h2 class="text-success">{{ low }}</h2>
+                    <h5>{{ item.description }}</h5>
+                    <h2 :class="`text-${item.visual_style}`">{{ item.count }}</h2>
                 </div>
             </div>
         </div>
     </div>
 </template>
-
+  
 <script>
     export default {
         props: ['events'],
@@ -36,21 +24,20 @@
             today() {
                 return this.events.data ? this.events.data.length : 0;
             },
-            high() {
-                return this.events.data 
-                    ? this.events.data.filter(item => item.threat_classification === "Alta").length 
-                    : 0;
-            },
-            medium() {
-                return this.events.data 
-                    ? this.events.data.filter(item => item.threat_classification === "Média").length 
-                    : 0;
-            },
-            low() {
-                return this.events.data 
-                    ? this.events.data.filter(item => item.threat_classification === "Baixa").length 
-                    : 0;
-            }
-        }
-    }
+            classificationSummary() {
+                const data = (this.events && this.events.data) ? this.events.data : [];
+                const map = data.reduce((acc, ev) => {
+                    const cls = ev.classification || {};
+                    const desc = cls.description || 'Não classificado';
+                    const visual = (cls.visual_style || 'secondary').toString().replace(/^text-/, '');
+                    if (!acc[desc]) {
+                        acc[desc] = { description: desc, count: 0, visual_style: visual };
+                    }
+                    acc[desc].count += 1;
+                    return acc;
+                }, {});
+            return Object.values(map).sort((a, b) => b.count - a.count);
+        },
+    },
+};
 </script>
