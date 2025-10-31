eea4a12a52a93902dbeaa7c641231982b2a09eb5
juliofraga <julio_fraga10@hotmail.com>
2025-10-30 23:16:14 -0300

    feat: Adding System Setting timezone_selected Handler

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use App\Models\system_setting;
+use Illuminate\Support\Facades\Config;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +25,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        $timezoneSetting = system_setting::where('attribute', 'timezone_selected')->first();
+        if ($timezoneSetting && $timezoneSetting->value) {
+            Config::set('app.timezone', $timezoneSetting->value);
+            date_default_timezone_set($timezoneSetting->value);
+        }
     }
 }
