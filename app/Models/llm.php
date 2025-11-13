ef0d15e280dda306d4efd9a3e0615293c1869809
juliofraga <julio_fraga10@hotmail.com>
2025-11-13 17:05:24 -0300

    feat: Preparing LLM Model

--- app/Models/llm.php
+++ app/Models/llm.php
@@ -8,4 +8,38 @@ use Illuminate\Database\Eloquent\Model;
 class llm extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'provider',
+        'model_id',
+        'api_base_url',
+        'api_key',
+        'max_tokens',
+        'default_temperature',
+        'context_length',
+        'pricing_prompt_token',
+        'pricing_completion_token',
+        'notes',
+        'active'
+    ];
+
+    public function rules(): array 
+    {
+        return [
+            'name' => 'required|unique:llm,name',
+            'provider' => 'required',
+            'model_id' => 'required',
+            'api_base_url' => 'required',
+            'api_key' => 'required'
+        ];
+    }
+
+    public function feedback(): array 
+    {
+        return [
+            'required' => 'O campo :attribute é obrigatório',
+            'name.unique' => 'Já existe um registro com esse nome. Informe outro nome, por favor.'
+        ];
+    }
 }
