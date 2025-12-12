ee106235f2a037b72d7167b625676104b69271b3
juliofraga <julio_fraga10@hotmail.com>
2025-12-12 20:21:41 -0300

    ref: Only apply filter when the button is clicked

--- resources/js/components/OverviewDashboard.vue
+++ resources/js/components/OverviewDashboard.vue
@@ -23,12 +23,6 @@ export default {
         this.applyRange();
     },
     watch: {
-        startdate() {
-            this.applyRange();
-        },
-        enddate() {
-            this.applyRange();
-        },
         totalbyday() {
             this.applyRange();
         },
