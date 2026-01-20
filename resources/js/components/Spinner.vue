0eadf0790a6934fb047fe63a0b20ef6210d0f121
juliofraga <julio_fraga10@hotmail.com>
2026-01-20 20:06:05 -0300

    feat: Adding translation for Spinner component

--- resources/js/components/Spinner.vue
+++ resources/js/components/Spinner.vue
@@ -2,12 +2,36 @@
     <div class="container">
         <div class="spinner-center">
             <div class="spinner"></div>
-            <label style="margin-top:1%;" class="text-white">Carregando...</label>
+            <label style="margin-top:1%;" class="text-white">{{ translation }}...</label>
         </div>
     </div>
 </template>
 
 <script>
     export default {
+        data() {
+			return {
+				translation: 'Loading'
+			};
+		},
+        methods: {
+            setText(locale) {
+                if (locale == 'pt_BR') {
+                    this.translation = 'Carregando';
+                } else if (locale == 'es') {
+                    this.translation = 'Cargando';
+                } else if (locale == 'fr') {
+                    this.translation = 'Chargement';
+                } else {
+                    this.translation = 'Loading';
+                }
+            }
+        },
+        mounted() {
+            const locale = document
+                .querySelector('meta[name="app-locale"]')
+                .getAttribute('content');
+            this.setText(locale);
+        }
     }
 </script>
